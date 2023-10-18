63a358473c1f9ddf326f18ced1dc59a4fbdba5b2
test <test>
2023-10-18 19:44:35 +0300

    Initial Commit

---
+++ NoteMeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoteMeProject
+//
+//  Created by user on 18.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
