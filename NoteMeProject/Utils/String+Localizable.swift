d17f05d7283423cd432e18c57589a811556a05b7
vlad <vlad>
2023-12-08 14:53:55 +0300

    Added localized methods for Strings

--- NoteMeProject/Utils/String+Localizable.swift
+++ NoteMeProject/Utils/String+Localizable.swift
@@ -5,4 +5,11 @@
 //  Created by Sergeenko on 08/12/2023.
 //
 
-import Foundation
+import UIKit
+
+extension String {
+    
+    func localized(bundle: Bundle = .main, tableName: String = "Localizable") -> String {
+        return NSLocalizedString(self, tableName: tableName, value: "", comment: "")
+    }
+}
