e20a8383815072628817206085370ad8b0e57fd6
vlad <vlad>
2023-12-08 13:23:37 +0300

    Configure LineTextField class

--- NoteMeProject/Custom/LineTextField.swift
+++ NoteMeProject/Custom/LineTextField.swift
@@ -5,4 +5,93 @@
 //  Created by Sergeenko on 08/12/2023.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class LineTextField: UIView {
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .appBoldFont.withSize(13.0)
+        label.textColor = .appText
+        label.textAlignment = .left
+        return label
+    }()
+    
+    private lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .none
+        textField.font = .appFont.withSize(15.0)
+        textField.textColor = .appText
+        textField.textAlignment = .left
+        return textField
+    }()
+    
+    private lazy var separator: UIView = {
+        let view = UIView()
+        view.backgroundColor = .appText
+        return view
+    }()
+    
+    private lazy var errorLabel: UILabel = {
+        let label = UILabel()
+        label.font = .appBoldFont.withSize(12.0)
+        label.textColor = .appRed
+        label.textAlignment = .left
+        return label
+    }()
+    
+    var title: String? {
+        get { titleLabel.text }
+        set { titleLabel.text = newValue }
+    }
+    
+    var errorText: String? {
+        get { errorLabel.text }
+        set { errorLabel.text = newValue }
+    }
+    
+    init() {
+        super.init(frame: .zero)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        setupUI()
+        setupConstraints()
+    }
+    
+    private func setupUI() {
+        addSubview(titleLabel)
+        addSubview(textField)
+        addSubview(separator)
+        addSubview(errorLabel)
+    }
+    
+    private func setupConstraints() {
+        titleLabel.snp.makeConstraints { make in
+            make.horizontalEdges.top.equalToSuperview()
+        }
+        
+        textField.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).inset(4.0)
+            make.horizontalEdges.equalToSuperview()
+        }
+        
+        separator.snp.makeConstraints { make in
+            make.top.equalTo(textField.snp.bottom).inset(4.0)
+            make.horizontalEdges.equalToSuperview()
+            make.height.equalTo(1.0)
+        }
+        
+        errorLabel.snp.makeConstraints { make in
+            make.bottom.horizontalEdges.equalToSuperview()
+            make.top.equalTo(separator.snp.bottom).inset(4.0)
+        }
+    }
+}
