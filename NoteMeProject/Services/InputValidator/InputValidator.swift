0d8cef3d0de889408af7715495188baeb5c34d0d
vlad <vlad>
2023-12-11 15:07:34 +0300

    Configure InputValidator

--- NoteMeProject/Services/InputValidator/InputValidator.swift
+++ NoteMeProject/Services/InputValidator/InputValidator.swift
@@ -5,4 +5,30 @@
 //  Created by Sergeenko on 11/12/2023.
 //
 
-import Foundation
+import UIKit
+
+
+final class InputValidator: NSObject {
+    
+    
+    func validate(email: String?) -> Bool {
+        return validate(string: email, pattern: "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+    }
+    
+    func validate(password: String?) -> Bool {
+        
+        return validate(string: password, pattern: "^.*(?=.{6,})(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*\\d)|(?=.*[!#$%&? \"]).*$")
+    }
+    
+    private func validate(string: String?, pattern: String) -> Bool {
+        guard let string,
+              let regex = try? NSRegularExpression(pattern: pattern,
+                                                   options: .caseInsensitive)
+        else {return false}
+        
+        let match = regex.firstMatch(in: string,
+                                     options: [],
+                                     range: NSRange(location: 0, length: string.count))
+        return match != nil
+    }
+}
